7f6e20a216e75cc86baf082b1ab781ec3bc7628c
Rito_Vega <vexin@proimpactmedia.com>
2025-09-25 18:32:33 -0500

    Update for Spots project array w/object pairs created for dynamic rendering

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,35 @@
 // -------------- Main Doc Selection Section ----------------
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
 const editProfile = document.querySelector(".profile__edit-button");
 const addPost = document.querySelector(".profile__add-button");
@@ -72,3 +103,10 @@ newPostCloseButton.addEventListener("click", () => {
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 newPostForm.addEventListener("submit", handleNewPostSubmit);
+
+// ------------------------------ New JS Project Section -------------------------------------------------------------------
+
+initialCards.forEach(function (item) {
+  console.log(item.name);
+  console.log(item.link);
+});
